c57be154833a64866e4f9f46f5b1e4411d018d0d
K-Steve <koffelsteve0@gmail.com>
2023-07-25 07:58:11 -0500

    added new lib

--- index.js
+++ index.js
@@ -1,12 +1,66 @@
 const express = require("express");
 const cors = require("cors");
+// const youtubedl = require("youtube-dl-exec");
+// const youtubedl = require("youtube-dl");
+// const ytSearch = require("yt-search");
+// const { exec } = require("youtube-dl-exec");
+const ytdl = require("ytdl-core");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/hello-nodejs", (req, res) => {
+app.get("/parse-url", async (req, res) => {
+  // youtubedl("https://www.youtube.com/watch?v=do3teoiyFho", {
+  //   format: "bestvideo+bestaudio",
+  //   dumpSingleJson: true,
+  //   noCheckCertificates: true,
+  //   noWarnings: true,
+  //   preferFreeFormats: true,
+  //   extractAudio: true,
+  //   addHeader: ["referer:youtube.com", "user-agent:googlebot"],
+  // }).then((output) => {
+  //   console.log("type of data is", typeof output);
+  //   console.log("keys of data", Object.keys(output));
+  //   const formats = output?.formats.map((item) => ({
+  //     format: item?.format,
+  //     isAudio: item?.audio_ext,
+  //   }));
+  //   console.log(output?.requested_formats);
+
+  // });
+
+  try {
+    const info = await ytdl.getInfo(
+      "https://www.youtube.com/watch?v=gLuhrhEfQBc"
+    );
+
+    console.log(
+      "formats are here",
+      info?.formats
+        .filter(
+          (item) => item?.audioCodec !== null && item?.qualityLabel !== null
+        )
+        .map((item) => ({
+          quality: item?.qualityLabel,
+        }))
+    );
+
+    // Find the best format that combines both video and audio (highest audio quality available)
+    const bestFormat = ytdl.chooseFormat(info.formats, {
+      filter: "audioandvideo",
+    });
+
+    if (!bestFormat) {
+      throw new Error("No format found with video and audio.");
+    }
+
+    console.log("Downloadable URL:", bestFormat);
+  } catch (error) {
+    console.error("Error getting URL:", error.message);
+  }
+
   res.send("hello");
 });
 

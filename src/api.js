c40e1c8e9d78aebc9f2cbccb95109118c68f72e9
maramzan <arsalanramzan811@gmail.com>
2023-07-27 18:11:26 +0500

    code formating

--- src/api.js
+++ src/api.js
@@ -11,24 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 router.get("/parse-url", async (req, res) => {
-  // youtubedl("https://www.youtube.com/watch?v=do3teoiyFho", {
-  //   format: "bestvideo+bestaudio",
-  //   dumpSingleJson: true,
-  //   noCheckCertificates: true,
-  //   noWarnings: true,
-  //   preferFreeFormats: true,
-  //   extractAudio: true,
-  //   addHeader: ["referer:youtube.com", "user-agent:googlebot"],
-  // }).then((output) => {
-  //   console.log("type of data is", typeof output);
-  //   console.log("keys of data", Object.keys(output));
-  //   const formats = output?.formats.map((item) => ({
-  //     format: item?.format,
-  //     isAudio: item?.audio_ext,
-  //   }));
-  //   console.log(output?.requested_formats);
-
-  // });
+ 
   const { url } = req.query;
   try {
     const info = await ytdl.getInfo(url);
@@ -37,7 +20,7 @@ router.get("/parse-url", async (req, res) => {
         (item) => item?.audioCodec !== null && item?.qualityLabel !== null
       )
     console.log("formats are here",formats );
-    res.send(formats.map(item=>{return {item}}));
+    res.send(formats);
 
   } catch (error) {
     console.error("Error getting URL:", error.message);

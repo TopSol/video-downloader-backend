d8d5e6a3a6b80531c0b00c203a7cf14f6c805e11
maramzan <arsalanramzan811@gmail.com>
2023-07-27 12:34:19 +0500

    url pass through parems

--- src/api.js
+++ src/api.js
@@ -9,10 +9,7 @@ const router = express.Router();
 
 app.use(cors());
 app.use(express.json());
-const bodyParser = require('body-parser');
 
-// Middleware to parse JSON data from the request body
-app.use(bodyParser.json());
 router.get("/parse-url", async (req, res) => {
   // youtubedl("https://www.youtube.com/watch?v=do3teoiyFho", {
   //   format: "bestvideo+bestaudio",
@@ -32,7 +29,7 @@ router.get("/parse-url", async (req, res) => {
   //   console.log(output?.requested_formats);
 
   // });
-  const { url } = req.body;
+  const { url } = req.query;
   try {
     const info = await ytdl.getInfo(url);
     const formats = info?.formats
@@ -40,18 +37,8 @@ router.get("/parse-url", async (req, res) => {
         (item) => item?.audioCodec !== null && item?.qualityLabel !== null
       )
     console.log("formats are here",formats );
-    res.send(formats.map(item=>{return {url:item.url}}));
+    res.send(formats.map(item=>{return {item}}));
 
-    // Find the best format that combines both video and audio (highest audio quality available)
-    const bestFormat = ytdl.chooseFormat(info.formats, {
-      filter: "audioandvideo",
-    });
-
-    if (!bestFormat) {
-      throw new Error("No format found with video and audio.");
-    }
-
-    console.log("Downloadable URL:", bestFormat);
   } catch (error) {
     console.error("Error getting URL:", error.message);
   }
